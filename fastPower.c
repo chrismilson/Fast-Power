b5d91608b3d4caac44c529ca09b4ecd754269d16
chrismilson <chris.w.milson@gmail.com>
2016-05-08 15:08:25 +1200

    finish calculator method

--- fastPower.c
+++ fastPower.c
@@ -2,26 +2,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+long long fastPow(int, int, int);
 
-
-int int main(int argc, char const *argv[]) {
-  int testFlag = 0;
-  int modulo, base, exponent;
+int main(int argc, char **argv) {
+  int modulo = 1, base = 1, exponent = 1;
   int c;
-  int *basePowers;
 
-  while ((c = getopt(argc, argv, "t")) != -1) {
+
+  while ((c = getopt(argc, argv, "b:e:m:")) != -1) {
     switch(c) {
-      case 't':
-        testFlag = 1;
+      case 'b':
+        base = strtol(optarg, NULL, 10);
+        break;
+      case 'e':
+        exponent = strtol(optarg, NULL, 10);
+        break;
+      case 'm':
+        modulo = strtol(optarg, NULL, 10);
         break;
       default:
+        break;
     }
   }
 
-  if (testFlag) {
-    base = 873;
-  }
+  printf("answer is %lld\n", fastPow(base, exponent, modulo));
 
   return 0;
+}
+
+long long fastPow(int a, int n, int m) {
+  long long answer = 1;
+  long long expo = n;
+  long long base = a;
+  while(expo > 0) {
+      if (expo & 1) {
+        answer *= base;
+        answer %= m;
+      }
+      base *= base;
+      base %= m;
+      expo >>= 1;
+  }
+  return answer;
 }
